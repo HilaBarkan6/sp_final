ea8ef6107285bc753f46d92a93c536d0b43983b5
Hila <hilabarkan.hb@gmail.com>
2024-05-20 12:50:55 +0300

    updated analysis file

--- analysis.py
+++ analysis.py
@@ -6,6 +6,22 @@ sys.path.insert(1, 'C:\Projects\Software Project\sp_hw1\Python')
 import kmeans as hw1
 
 def main():
+    if len(sys.argv) != 3:
+        print("An Error Has Occurred") 
+        exit()
+    k=0
+    try:
+        k = int(sys.argv[1])
+    except:
+        print("An Error Has Occurred\n")
+        exit()
+    file_path = sys.argv[2]
+
+    vectors = hw1.create_vectors(file_path)
+    final_centroids = hw1.calulate_k_means_final_project(vectors, k, 200, 0.01)
+   
+
+
     X, y = make_blobs(random_state=42)
     kmeans = KMeans(n_clusters=2, random_state=42)
     silhouette_score(X, kmeans.fit_predict(X))
